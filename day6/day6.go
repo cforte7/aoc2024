18570201edb3168911ed8735a884130ff1320dda
Chris Forte <cforte7@gmail.com>
2024-12-07 08:43:24 -0600

    part two wip

--- day6/day6.go
+++ day6/day6.go
@@ -43,7 +43,50 @@ func rotateDir(dir [2]int) [2]int {
 	return [2]int{dir[1], -dir[0]}
 }
 
-func dayOne(data [][]string) int {
+func isCycle(data [][]string, obs [2]int) int {
+	dupe := makeDuplicate(data)
+	dupe[obs[0]][obs[1]] = "#"
+	visited := make(map[[4]int]bool)
+
+	pos := findStart(dupe)
+	dir := [2]int{-1, 0}
+	for !leavingMap(pos, dir, dupe) {
+		posAndDir := [4]int{pos[0], pos[1], dir[0], dir[1]}
+
+		_, weveBeenHere := visited[posAndDir]
+		if weveBeenHere {
+			return 1
+		}
+		visited[posAndDir] = true
+		if !canContinue(pos, dir, dupe) {
+			dir = rotateDir(dir)
+		}
+		pos = getNextPost(pos, dir)
+	}
+
+	return 0
+}
+
+func makeDuplicate(data [][]string) [][]string {
+	duplicate := make([][]string, len(data))
+	for i := range data {
+		duplicate[i] = make([]string, len(data[i]))
+		copy(duplicate[i], data[i])
+	}
+	return duplicate
+}
+
+func dayTwo(data [][]string, visited map[[2]int]bool) int {
+	start := findStart(data)
+	delete(visited, start)
+	total := 0
+	for key := range visited {
+		total += isCycle(data, key)
+	}
+	return total
+}
+
+func dayOne(data [][]string) (int, map[[2]int]bool) {
 	pos := findStart(data)
 	dir := [2]int{-1, 0}
 	visited := make(map[[2]int]bool)
@@ -56,11 +99,16 @@ func dayOne(data [][]string) int {
 	}
 
 	visited[[2]int{pos[0], pos[1]}] = true
-	return len(visited)
+	return len(visited), visited
 }
 
 func main() {
 	data := helpers.Txt_to_lines("input.txt")
 	asChars := helpers.StringsToChars(data)
-	fmt.Println(dayOne(asChars))
+
+	res, visited := dayOne(asChars)
+	res2 := dayTwo(asChars, visited)
+	fmt.Println(res, res2)
 }
+
+// 1938 is too high
